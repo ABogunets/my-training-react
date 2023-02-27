d50f141da50afe2d4db76373c517f06f93ea1806
ABogunets <abogunets@gmail.com>
2023-02-27 10:08:16 +0200

    at start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+// * Text below was by default after react package install
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = React.createElement('div', { a: 5, b: 10, children: 'Привет мир' });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// ReactDOM.render(element, document.querySelector('#root'));
+
+const jsxElement = <div>Hello world</div>;
+console.log('jsxElement :>> ', jsxElement);
+
+ReactDOM.createRoot(document.getElementById("root")).render(jsxElement);

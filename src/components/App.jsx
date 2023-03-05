0519bfbe27488adec8dd7cd45c5c0271b951d7f0
ABogunets <abogunets@gmail.com>
2023-03-06 00:05:37 +0200

    example done

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// * from Repeta lecture1.2-styles
+import { PageTitle } from "components/PageTitle/PageTitle";
+import { EventBoard } from "components/EventBoard/EventBoard";
+import upcomingEvents from 'upcoming-events.json';
+
+export const App = () => {
+  return (
+    <>
+    <PageTitle text='24th Core Worlds Coalition Conference' />
+    <EventBoard events={upcomingEvents} />
+    </>
+  );
+};
+
+
+

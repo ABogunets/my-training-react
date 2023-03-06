94b2c9281053555b45f66af487392285e433876c
ABogunets <abogunets@gmail.com>
2023-03-06 23:34:24 +0200

    emotions done

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  position: relative;
+  border: 2px dashed black;
+  padding: 8px;
+  border-radius: 4px;
+`;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from 'components/App';
+import { App } from 'components/App/App';
+import { ThemeProvider } from '@emotion/react'
+import { theme } from 'constants'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    < ThemeProvider theme={theme}>
+      <App />
+    </ ThemeProvider>
   </React.StrictMode>
 );
 
